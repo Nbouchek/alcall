a0ef5b55c4bce1592704ac4bd820327c00d7b578
Nbouchek <nbouchek@gmail.com>
2025-06-23 23:30:03 +0100

    Remove 'Connected to' Auth and Messages URLs from login page UI

--- web/frontend/pages/index.js
+++ web/frontend/pages/index.js
@@ -240,10 +240,6 @@ export default function Home() {
                 >
                   Login
                 </button>
-                <div className="mt-2 p-2 bg-blue-50 rounded text-xs text-blue-600">
-                  Connected to: Auth: {AUTH_API_BASE_URL} | Messages:{" "}
-                  {MESSAGE_API_BASE_URL}
-                </div>
               </form>
             </div>
           )}
