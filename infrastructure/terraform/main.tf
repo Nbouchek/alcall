0d5653823d5021865ac75be5e1feff7324173d2d
Nbouchek <nbouchek@gmail.com>
2025-07-12 20:32:13 +0100

    update]

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -16,23 +16,23 @@ resource "render_postgres" "alcall_db" {
   name    = "alcall-postgres-db"
   plan    = "free"
   region  = "frankfurt"
-  version = "14"
+  version = "16"
 
   database_name = var.postgres_db_name
   database_user = var.postgres_db_user
 }
 
 resource "render_web_service" "auth_service" {
-  name               = "alcall-auth-service"
-  plan               = "starter"
-  region             = "frankfurt"
+  name    = "alcall-auth-service"
+  plan    = "starter"
+  region  = "frankfurt"
 
   runtime_source = {
     docker = {
-      auto_deploy   = true
-      branch        = "repo-setup-fixes"
-      repo_url      = "https://github.com/Nbouchek/alcall"
-      root_directory = "services/auth-service"
+      auto_deploy      = true
+      branch           = "repo-setup-fixes"
+      repo_url         = "https://github.com/Nbouchek/alcall"
+      root_directory   = "services/auth-service"
     }
   }
 
@@ -42,16 +42,16 @@ resource "render_web_service" "auth_service" {
 }
 
 resource "render_web_service" "janus_service" {
-  name               = "alcall-janus-service"
-  plan               = "starter"
-  region             = "frankfurt" # Following user preference [[memory:3058791]]
+  name    = "alcall-janus-service"
+  plan    = "starter"
+  region  = "frankfurt"
 
   runtime_source = {
     docker = {
-      auto_deploy   = true
-      branch        = "repo-setup-fixes"
-      repo_url      = "https://github.com/Nbouchek/alcall"
-      root_directory = "services/janus-service"
+      auto_deploy      = true
+      branch           = "repo-setup-fixes"
+      repo_url         = "https://github.com/Nbouchek/alcall"
+      root_directory   = "services/janus-service"
     }
   }
 }
