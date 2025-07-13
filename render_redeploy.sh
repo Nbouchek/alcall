e740d2bf8deff04fdb84bee6b3190ee5816f2dab
Nbouchek <nbouchek@gmail.com>
2025-07-13 23:20:21 +0100

    fix

--- render_redeploy.sh
+++ render_redeploy.sh
@@ -89,7 +89,7 @@ render services create web \
   --env PORT=3000 \
   --env NEXT_PUBLIC_AUTH_API_URL=https://alcall-auth.onrender.com/api/v1 \
   --env NEXT_PUBLIC_MESSAGE_API_URL=https://alcall-message.onrender.com/api/v1/messages \
-  --env NEXT_PUBLIC_REALTIME_API_URL=wss://alcall-realtime.onrender.com \
+  --env NEXT_PUBLIC_REALTIME_API_URL=wss://alcall-realtime-service.onrender.com/realtime \
   --env NEXT_PUBLIC_JANUS_URL=wss://alcall-janus.onrender.com/janus \
   --env NEXT_PUBLIC_JANUS_HTTP_URL=https://alcall-janus.onrender.com \
   --env NEXT_PUBLIC_FORCE_NORMAL_MODE=false \
