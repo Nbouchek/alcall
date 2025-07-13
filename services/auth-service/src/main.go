623ee82618944e7f06a77cd517508e4f286a1197
Nbouchek <nbouchek@gmail.com>
2025-07-13 08:34:07 +0100

    wbe

--- services/auth-service/src/main.go
+++ services/auth-service/src/main.go
@@ -120,7 +120,7 @@ func main() {
 
 	// Standardized CORS configuration
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"https://unifiedchat-frontend.onrender.com", "http://localhost:3000"}
+	config.AllowOrigins = []string{"https://alcall-frontend.onrender.com", "http://localhost:3000"}
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "X-Requested-With"}
 	config.ExposeHeaders = []string{"Content-Length"}
